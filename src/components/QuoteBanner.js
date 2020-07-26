4cb843f0b7d6e852c5fb8b8ef1bb4635dc4d54c5
Irene Perez <inreallifeirene@users.noreply.github.com>
2020-07-26 14:51:20 -0700

    quote - styling complete

--- src/components/QuoteBanner.js
+++ src/components/QuoteBanner.js
@@ -5,8 +5,9 @@ import { sapphire } from "../styles"
 
 const Quote = () => {
   return (
-    <Wrapper>
+    <Wrapper className="row col-md-12 d-flex flex-column align-items-center">
       <h2>"The most beautiful makeup of a woman is passion. But cosmetics are easier to buy."</h2>
+      <br />
       <p>~ <em>Yves Saint-Laurent</em></p>
     </Wrapper>
   )
@@ -14,6 +15,10 @@ const Quote = () => {
 
 const Wrapper = styled.div`
   background-color: ${sapphire};
+
+  padding: 25px;
+
+  color: white;
 `
 
 export default Quote
