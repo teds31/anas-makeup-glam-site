1e850ba34fc1744aeba080749008f195333ab0f5
teds31 <tstanowski31@gmail.com>
2022-05-05 15:27:36 -0700

    update packages and removecontact form and replace with call and email links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Intro from "../components/Intro"
@@ -8,7 +8,9 @@ import Mini from "../components/portfolio/MiniPortfolio"
 import Testimonials from "../components/Testimonials"
 import Services from "../components/services/Services"
 import Quote from "../components/QuoteBanner"
-import Contact from "../components/Contact"
+// import Contact from "../components/Contact"
+import {purple } from "../styles"
+
 
 export default () => {
   return (
@@ -21,15 +23,27 @@ export default () => {
         <Testimonials />
         <Services />
         <Quote />
-        <div className="d-flex justify-content-center flex-column text-center">
-          <p>
-            <a href="tel:702-381-3158">(702) 347-0858</a> |{" "}
-            <a href="mailto: booking@anasmakeupglam.com">
+        <div className="d-flex justify-content-center flex-column text-center pt-4 pb-4">
+                <h1 style={{color: `${purple}`, marginBottom: "15px", fontSize: "3rem"}}>Contact Me</h1>
+
+          <p>Ready to get glammed up or have any questions? Call or send me a message!</p>
+          <Wrapper id="contact-links">
+            <a class="phone-link" href="tel:702-381-3158">(702) 347-0858</a>
+            <br /><br />
+
+            <a class="email-link" href="mailto: booking@anasmakeupglam.com">
               booking@anasmakeupglam.com
             </a>
-          </p>
+            </Wrapper>
         </div>
       </main>
     </Layout>
   )
 }
+
+const Wrapper = styled.div`
+.phone-link, .email-link {
+  font-size: 1.5rem;
+  padding: 2rem 0;
+};
+`
